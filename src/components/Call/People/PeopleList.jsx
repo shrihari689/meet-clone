97b72a62abddd11328f2d1e8b1e1d07c923163df
Shri Hari L <57325503+shrihari689@users.noreply.github.com>
2021-06-04 19:56:46 +0530

    🎨 Fix Sidebar Height Padding

--- src/components/Call/People/PeopleList.jsx
+++ src/components/Call/People/PeopleList.jsx
@@ -48,37 +48,39 @@ const PeopleList = ({ onClose }) => {
     return (
         <>
             <SidebarHeader title="People" onClose={onClose} />
-            <div className="grid grid-cols-1 md:grid-cols-3 my-3 w-full px-2">
-                <div className="flex flex-col space-y-1 items-center cursor-pointer rounded-md px-2 py-3 select-none text-gray-700 hover:bg-gray-100">
-                    <i className="google-material-icons" style={{ fontSize: '18px' }}>mic_off</i>
-                    <span className="text-xs text-center">All muted</span>
+            <div className="flex flex-col flex-1 overflow-y-auto items-center w-full">
+                <div className="grid grid-cols-1 md:grid-cols-3 w-full px-2">
+                    <div className="flex flex-col space-y-1 items-center cursor-pointer rounded-md px-2 py-3 select-none text-gray-700 hover:bg-gray-100">
+                        <i className="google-material-icons" style={{ fontSize: '18px' }}>mic_off</i>
+                        <span className="text-xs text-center">All muted</span>
+                    </div>
+                    <div className="flex flex-col space-y-1 items-center cursor-pointer rounded-md px-2 py-3 select-none text-gray-700 hover:bg-gray-100">
+                        <i className="google-material-icons" style={{ fontSize: '18px' }}>person_add</i>
+                        <span className="text-xs text-center">Add People</span>
+                    </div>
+                    <div className="flex flex-col space-y-1 items-center cursor-pointer rounded-md px-2 py-3 select-none text-gray-700 hover:bg-gray-100">
+                        <i className="google-material-icons" style={{ fontSize: '18px' }}>toggle_on</i>
+                        <span className="text-xs text-center">Host Control</span>
+                    </div>
                 </div>
-                <div className="flex flex-col space-y-1 items-center cursor-pointer rounded-md px-2 py-3 select-none text-gray-700 hover:bg-gray-100">
-                    <i className="google-material-icons" style={{ fontSize: '18px' }}>person_add</i>
-                    <span className="text-xs text-center">Add People</span>
-                </div>
-                <div className="flex flex-col space-y-1 items-center cursor-pointer rounded-md px-2 py-3 select-none text-gray-700 hover:bg-gray-100">
-                    <i className="google-material-icons" style={{ fontSize: '18px' }}>toggle_on</i>
-                    <span className="text-xs text-center">Host Control</span>
-                </div>
+                <section className="w-full px-3 pl-4 flex-1">
+                    <h2 className="font-semibold my-2" style={{ fontSize: "0.7rem" }}>IN CALL</h2>
+                    <div className="flex flex-col">
+                        {
+                            PEOPLES
+                                .sort((a, b) => a.name < b.name ? -1 : 1)
+                                .map((people, i) =>
+                                    < PeopleItem
+                                        key={i}
+                                        details={people}
+                                        onMute={(_) => { }}
+                                        onPin={(_) => { }}
+                                    />
+                                )
+                        }
+                    </div>
+                </section>
             </div>
-            <section className="w-full px-3 pl-4 flex-1 overflow-y-auto">
-                <h2 className="font-semibold my-2" style={{ fontSize: "0.7rem" }}>IN CALL</h2>
-                <div className="flex flex-col">
-                    {
-                        PEOPLES
-                            .sort((a, b) => a.name < b.name ? -1 : 1)
-                            .map((people, i) =>
-                                < PeopleItem
-                                    key={i}
-                                    details={people}
-                                    onMute={(_) => { }}
-                                    onPin={(_) => { }}
-                                />
-                            )
-                    }
-                </div>
-            </section>
         </>
     );
 }
--- src/components/Call/Shared/SidebarHeader.jsx
+++ src/components/Call/Shared/SidebarHeader.jsx
@@ -1,6 +1,6 @@
 const SidebarHeader = ({ title, onClose }) => {
     return (
-        <div className="flex items-center justify-between w-full px-3">
+        <div className="flex items-center justify-between w-full px-3 pb-4">
             <h3 className="text-md ml-2">{title}</h3>
             <div onClick={onClose} className="h-8 w-8 flex items-center justify-center rounded-full cursor-pointer hover:bg-gray-200">
                 <i className="material-icons" style={{ fontSize: '18px' }}>close</i>
