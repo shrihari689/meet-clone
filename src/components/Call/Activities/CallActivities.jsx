93964bb5a60c5f48a4dfa410e98ff8fce3eea415
Shri Hari L <57325503+shrihari689@users.noreply.github.com>
2021-06-04 23:04:30 +0530

    🖼 Add Alt to Image

--- src/components/Call/Activities/CallActivities.jsx
+++ src/components/Call/Activities/CallActivities.jsx
@@ -15,7 +15,7 @@ const CallActivities = ({ onClose }) => {
                 {
                     ACTIVITIES.map(({ title, description, image }, id) =>
                         <div key={id} className="flex items-center cursor-pointer w-full px-3 py-2 my-1 hover:bg-gray-100">
-                            <img className="w-10 h-10 rounded-full" src={image} alt="Recording" />
+                            <img className="w-10 h-10 rounded-full" src={image} alt={title} />
                             <div className="ml-3 flex flex-col">
                                 <span className="text-sm font-semibold mb-1">{title}</span>
                                 <span className="text-xs text-gray-500">{description}</span>
