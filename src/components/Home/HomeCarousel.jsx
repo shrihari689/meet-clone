194becf0450cefc82d4c934b35b37dbc61f48302
Shri Hari L <57325503+shrihari689@users.noreply.github.com>
2021-06-05 15:17:01 +0530

    🏠 Improve Home Page

---
+++ src/components/Home/HomeCarousel.jsx
@@ -0,0 +1,42 @@
+import link from "./../../assets/home_slider_link.svg";
+import everyone from "./../../assets/home_slider_everyone.svg";
+import plan from "./../../assets/home_slider_plan.svg";
+import safe from "./../../assets/home_slider_safe.svg";
+import { useEffect, useState } from "react";
+
+const CONTENTS = [
+    { image: link, title: "Get a link that you can share", desc: "Click New meeting to get a link that you can send to people that you want to meet with" },
+    { image: everyone, title: "See everyone together", desc: "To see more people at the same time, go to Change layout in the More options menu" },
+    { image: plan, title: "Plan ahead", desc: "Click New meeting to schedule meetings in Google Calendar and send invitations to participants" },
+    { image: safe, title: "Your meeting is safe", desc: "No one can join a meeting unless invited or admitted by the host" }
+]
+
+
+const HomeCarousel = () => {
+
+    const [currentSlide, setCurrentSlide] = useState(0);
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setCurrentSlide(prev => {
+                if (prev === (CONTENTS.length - 1)) return 0;
+                return prev + 1;
+            })
+        }, 2000)
+        return () => {
+            clearInterval(intervalId);
+        }
+    }, [])
+
+    return (
+        <div className="flex justify-center">
+            <div className="flex flex-col items-center">
+                <img className="w-1/3 rounded-full object-cover" src={CONTENTS[currentSlide].image} alt="" />
+                <span className="text-base mt-2 text-center">{CONTENTS[currentSlide].title}</span>
+                <span className="text-gray-600 text-xs mt-2 px-60 text-center">{CONTENTS[currentSlide].desc}</span>
+            </div>
+        </div>
+    );
+}
+
+export default HomeCarousel;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,5 @@
+import HomeCarousel from "../components/Home/HomeCarousel"
+
 const HomePage = () => {
     return (
         <div>
@@ -27,7 +29,7 @@ const HomePage = () => {
                     <img className="h-8 w-8 rounded-full ml-2" src="https://lh3.google.com/u/1/ogw/ADGmqu9eAv3S4CI3NA-MHUQJ_vUrZr7geEk-MnlgcZpa=s32-c-mo" alt="Shri Hari L" />
                 </div>
             </nav>
-            <main className="mt-10 md:mt-24 flex items-center flex-wrap">
+            <main className="mt-10 md:h-96 md:mt-24 flex items-center flex-wrap">
                 <div className="w-full md:w-1/2 flex flex-col items-center md:items-start px-5 md:px-10">
                     <h1 className="text-xl md:text-3xl md:pr-10 font-medium">Secure Video Call Conferencing for everyone</h1>
                     <span className="text-xs md:text-sm text-gray-600 mt-2 md:pr-20">Connect, collaborate, and celebrate from anywhere with Google Meet</span>
@@ -43,28 +45,7 @@ const HomePage = () => {
                     </div>
                 </div>
                 <div className="w-full md:w-1/2 pt-8 px-2">
-                    <div className="flex justify-center">
-                        <div className="flex flex-col items-center">
-                            <img className="w-1/3 rounded-full object-cover" src="https://www.gstatic.com/meet/user_edu_get_a_link_light_90698cd7b4ca04d3005c962a3756c42d.svg" alt="" />
-                            <span className="text-base mt-2 text-center">Get a link that you can share</span>
-                            <span className="text-xs mt-2 px-20 text-center">Click <b>New meeting</b> to get a link that you can send to people that you want to meet with</span>
-                        </div>
-                        <div className="hidden flex-col items-center">
-                            <img className="w-1/3 rounded-full object-cover" src="https://www.gstatic.com/meet/user_edu_brady_bunch_light_81fa864771e5c1dd6c75abe020c61345.svg" alt="" />
-                            <span className="text-base mt-2 text-center">See everyone together</span>
-                            <span className="text-xs mt-2 px-20 text-center">To see more people at the same time, go to Change layout in the More options menu</span>
-                        </div>
-                        <div className="hidden flex-col items-center">
-                            <img className="w-1/3 rounded-full object-cover" src="https://www.gstatic.com/meet/user_edu_scheduling_light_b352efa017e4f8f1ffda43e847820322.svg" alt="" />
-                            <span className="text-base mt-2 text-center">Plan ahead</span>
-                            <span className="text-xs mt-2 px-20 text-center">Click <b>New meeting</b> to schedule meetings in Google Calendar and send invitations to participants</span>
-                        </div>
-                        <div className="hidden flex-col items-center">
-                            <img className="w-1/3 rounded-full object-cover" src="https://www.gstatic.com/meet/user_edu_safety_light_e04a2bbb449524ef7e49ea36d5f25b65.svg" alt="" />
-                            <span className="text-base mt-2 text-center">Your meeting is safe</span>
-                            <span className="text-xs mt-2 px-20 text-center">No one can join a meeting unless invited or admitted by the host</span>
-                        </div>
-                    </div>
+                    <HomeCarousel />
                 </div>
             </main>
         </div>
