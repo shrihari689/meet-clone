34ba07cd4e49c00e438e1551f6af29bc1673d0b5
Shri Hari L <57325503+shrihari689@users.noreply.github.com>
2021-06-06 16:05:06 +0530

    📱 Add Call Actions

--- src/database/auth.js
+++ src/database/auth.js
@@ -1,16 +1,16 @@
 import { createSlice } from "@reduxjs/toolkit"
+import { User } from "./entities"
 
 
 const authSlice = createSlice({
     name: "Auth",
     initialState: {
-        email: "",
-        name: "",
-        image: "",
+        ...User,
         isLoggedIn: null,
     },
     reducers: {
         setUser: (state, { payload }) => {
+            state.id = payload.id
             state.name = payload.name
             state.email = payload.email
             state.image = payload.image
---
+++ src/database/entities.js
@@ -0,0 +1,6 @@
+export const User = {
+    id: "",
+    name: "",
+    email: "",
+    image: ""
+}
