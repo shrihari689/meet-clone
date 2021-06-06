a09f0b0aee65abe9e15d8bd2af90568ca541666f
Shri Hari L <57325503+shrihari689@users.noreply.github.com>
2021-06-07 02:13:31 +0530

    ➕ Add Backend Url

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,5 +1,7 @@
 import { io } from "socket.io-client"
+import { isDevMode } from "./general"
 
-const socket = io("http://localhost:4000/")
+const backendURL = isDevMode() ? "http://localhost:4000" : process.env.REACT_APP_BACKEND_URL
+const socket = io(backendURL)
 
 export default socket;
